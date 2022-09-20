33ec0f605b7eab686773c34a29a14548461b8ed4
Alexander Garcia <alexander.garcia21400@gmail.com>
2022-09-20 09:36:16 -0500

    Added code to kotlin

---
+++ src/main/kotlin/week02/marketing.kt
@@ -0,0 +1,10 @@
+package week02
+
+fun main(args: Array<String>){
+    val friendsCount=450
+    if (friendsCount>500)
+        println("Adopt a dog today!!!")
+        else if (friendsCount<500)
+            println("Adopt a cat today!!!")
+
+}
