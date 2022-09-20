76f26c82db06eb84dbb751ed765c41c20819be2f
Alexander Garcia <alexander.garcia21400@gmail.com>
2022-09-20 09:49:58 -0500

    Added code to kotlin

---
+++ src/main/kotlin/week02/NameAgeAnnualincome.kt
@@ -0,0 +1,8 @@
+package week02
+
+        fun main(){
+            var age = 26
+            var income = 100000.0
+            var name = "Joe Mahoney"
+            println("My name is $name, my age is $age and \nI hope to earn $income per year")
+        }
