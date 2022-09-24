37b3767cf52a49f1afac9394614c4e27a458d43a
Alexander Garcia <alexander.garcia21400@gmail.com>
2022-09-24 14:06:52 -0500

    Added code to kotlin

--- src/main/kotlin/week03/sorty.kt
+++ src/main/kotlin/week03/sorty.kt
@@ -1,9 +1,9 @@
 fun main(){
-    println("Name one:")
+    println("Enter name on3")
     var name1 = readLine()!!
-    println("Name two:")
+    println("Enter name two:")
     var name2 = readLine()!!
-    println("Name three:")
+    println("Enter name three:")
     var name3 = readLine()!!
 
    if ((name1.compareTo(name2)) < 0 && (name1.compareTo(name3)) < 0){
@@ -12,8 +12,8 @@ fun main(){
            println(name2);
            println(name3);
        } else {
-           println(name2);
            println(name3);
+           println(name2);
        }
    }
     else if ((name2.compareTo(name3)) < 0 && (name2.compareTo(name1)) < 0){
