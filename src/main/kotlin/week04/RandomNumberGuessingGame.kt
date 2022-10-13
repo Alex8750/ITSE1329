ccb260ce19f21a496c3763ef81f0de5fa4adc69e
Alexander Garcia <alexander.garcia21400@gmail.com>
2022-10-13 09:30:55 -0500

    Added code to kotlin

--- src/main/kotlin/week04/RandomNumberGuessingGame.kt
+++ src/main/kotlin/week04/RandomNumberGuessingGame.kt
@@ -2,7 +2,17 @@ package week04
 import kotlin.random.Random
 fun main(){
     val random = Random.nextInt(0,100)
-    println(random)
     println("Guess the random number: ")
     var guess = Integer.valueOf(readLine())
+    while(guess != random){
+        if (guess < random)
+            println("Number was too low")
+        else if (guess > random)
+            println("Number was too high")
+        println("Try again")
+        guess = Integer.valueOf(readLine())
+    }
+    if (guess == random){
+        println("Correct the number was $random")
+    }
 }
